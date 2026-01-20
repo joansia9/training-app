f4af13f2d55f3cd72a9c22e8d255fc69aa9b9525
joansia9 <joansia9@byu.edu>
2026-01-20 20:34:57 +0000

    phase 6

---
+++ src/app/pages/admin/admin.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.page.html',
+  styleUrl: './admin.page.scss',
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AdminPage {}
---
+++ src/app/pages/tasks/tasks.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.page.html',
+  styleUrl: './tasks.page.scss',
+})
+export class TasksPage {
+
+}
