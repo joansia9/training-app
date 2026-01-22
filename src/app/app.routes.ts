f528a345e49ed934f8c306e66844b82b6c201138
joansia9 <joansia9@byu.edu>
2026-01-22 18:53:26 +0000

    up to error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,7 +23,7 @@ export const routes: Routes = [
         canActivate: [permissionGuard<Permission>(['view-secret'])],
       },
       {
-        path: 'user',
+        path: 'task',
         component: TasksPage,
         canActivate: [permissionGuard<Permission>(['manage-task'])],
       },
--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TRPC_CLIENT } from '../../utils/trpc.client';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss',
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class TasksPage {}
+export class TasksPage {
+  private readonly trpc = inject(TRPC_CLIENT); //puts trpc client token here
+
+  //pagination stuff
+  protected readonly PAGE_SIZE = 12 as const;
+  private readonly pageOffset = signal(0); //signal getter functions - reads values
+
+  protected readonly taskResource = trpcResource(
+    this.trpc.task.getTasksByUser.mutate,
+    () => ({
+      pageSize: this.PAGE_SIZE,
+      pageXOffset: this.pageOffset(),
+    }),
+    { autoRefresh: true }
+  );
+}
