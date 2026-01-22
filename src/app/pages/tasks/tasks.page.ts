dd5e719fdf15ef86bffa52876574c6197e959fd0
joansia9 <joansia9@byu.edu>
2026-01-22 20:01:58 +0000

    What I have for tasks so far

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,10 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { TRPC_CLIENT } from '../../utils/trpc.client';
 import { trpcResource } from '@fhss-web-team/frontend-utils';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatPaginator],
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss',
 })
@@ -14,6 +16,9 @@ export class TasksPage {
   //pagination stuff
   protected readonly PAGE_SIZE = 12 as const;
   private readonly pageOffset = signal(0); //signal getter functions - reads values
+  protected handlePageEvent(e: PageEvent) {
+    this.pageOffset.set(e.pageIndex * e.pageSize);
+  }
 
   protected readonly taskResource = trpcResource(
     this.trpc.task.getTasksByUser.mutate,
