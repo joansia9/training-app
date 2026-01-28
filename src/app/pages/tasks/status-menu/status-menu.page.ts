08e4d93bb1d0973213058949d65a5dc7f81f6684
joansia9 <joansia9@byu.edu>
2026-01-28 20:36:55 +0000

    phase 7 css

---
+++ src/app/pages/tasks/status-menu/status-menu.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-menu',
+  imports: [],
+  templateUrl: './status-menu.page.html',
+  styleUrl: './status-menu.page.scss',
+})
+export class StatusMenuPage {
+
+}
---
+++ src/app/pages/tasks/tasks-card/tasks-card.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-card',
+  imports: [],
+  templateUrl: './tasks-card.page.html',
+  styleUrl: './tasks-card.page.scss',
+})
+export class TasksCardPage {
+
+}
